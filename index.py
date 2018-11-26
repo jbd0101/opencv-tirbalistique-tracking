4c8d7306e1f5ed4f6d266f2e1947229fa48617f1
jean-christophe bauduin <maildejc@hotmail.be>
2018-11-26 19:17:39 +0100

    l

--- index.py
+++ index.py
@@ -11,16 +11,20 @@ print("Version de opencv "+str(version))
 #read video file
 cap = cv2.VideoCapture(video_path)
 length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-fgbg = cv2.createBackgroundSubtractorMOG2(history=15,detectShadows=0)
+fgbg = cv2.createBackgroundSubtractorMOG2(history=0,detectShadows=0,varThreshold = 300)
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
+fps = cap.get(cv2.CAP_PROP_FPS)
+t = 0
 
 distanceX=200.0 #(en cm )
 distanceY=150.0 #(en cm )
-
+pointsCM = []
 distParPixelX =distanceX / frame_width
 distParPixelY =distanceY / frame_height
+print(distParPixelX)
+print(distParPixelY)
 # frame_width=float(cap.get(cv2.CAP_PROP_FRAME_WIDTH ))
 # frame_height=float(cap.get(cv2.CAP_PROP_FRAME_HEIGHT ))
 font = cv2.FONT_HERSHEY_SIMPLEX
@@ -41,11 +45,11 @@ while (cap.isOpened):
     fgmask = fgbg.apply(frame)
     # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     # mask = cv2.inRange(hsv, lower_red, upper_red)
-    # res = cv2.bitwise_and(frame,frame, mask= mask)
+    # res = cv2.bitwise_and(frame,fr380ame, mask= mask)
     (im2, contours, hierarchy) = cv2.findContours(fgmask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     # cv2.drawContours(frame, contours, -1, (255,255,0), 3)
     for c in contours:
-      if cv2.contourArea(c) < 80 or cv2.contourArea(c) > 380:
+      if cv2.contourArea(c) < 5 or cv2.contourArea(c) > 380:
         continue
       #get bounding box from countour
       (x, y, w, h) = cv2.boundingRect(c)
@@ -56,17 +60,24 @@ while (cap.isOpened):
       # cv2.imshow('rgb',frame)
       for pos in points:
         cv2.circle(frame,(int(pos[0]),int(pos[1])), 2, (0,0,255), 2)
-        cv2.putText(frame,str(round(pos[0]*distParPixelX,2))+"cm et "+str(round(pos[1]*distParPixelY,2)),(int(pos[0]+10),int(pos[1])), font, 0.3,(255,255,255),1,cv2.LINE_AA)
+        cv2.putText(frame,str(round(pos[0]*distParPixelX,0))+"cm et "+str(round(distanceY-(pos[1]*distParPixelY),0)),(int(pos[0]+10),int(pos[1])), font, 0.3,(255,255,255),1,cv2.LINE_AA)
 
 
       lastframe = frame
-      cv2.imshow('res',frame)
+      pointsCM.append([((x+(w/2.0))*distParPixelX),(distanceY-(y+(h/2.0)))])
       points.append([(x+(w/2.0)),(y+(h/2.0))])
-      time.sleep(0.1)
+      cv2.putText(frame,str(round((x+(w/2.0))*distParPixelX,2))+"cm et "+str(round(distanceY-((y+(h/2.0))*distParPixelY),2))+"cm",(10,10), font, 0.3,(255,255,255),1,cv2.LINE_AA)
+      t = nmbrF / fps *10
+      cv2.putText(frame,str(t)+"s",(frame_width-50,10), font, 0.3,(255,255,255),1,cv2.LINE_AA)
+
+      cv2.imshow('res',frame)
+      time.sleep(0.4)
+
 
       out.write(frame)
 
       if nmbrF>=(length-1):
+        print("point en pixels")
         x = []
         y = []
         for pos in points:
@@ -76,11 +87,28 @@ while (cap.isOpened):
         y = np.array(y)
         z = np.polyfit(x, y, 2)
         p = np.poly1d(z)
-        print("y="+str(z[1])+"x^2+"+str(z[0])+"x+"+str(z[2]))
-        for i in range(0,frame_width,6):
-          cv2.circle(frame,(int(i),int(p(i))), 2, (255,255,255), 2)
-        cv2.imshow('res',frame)
+        pv = np.polyder(p)
 
+
+
+        for i in range(0,frame_width,2):
+          cv2.circle(frame,(int(i),int(p(i))), 2, (255,255,255), 2)
+
+        print("point en cm")
+        x = []
+        y = []
+        for pos in pointsCM:
+          x.append(int(pos[0]))
+          y.append(int(pos[1]))
+        x = np.array(x)
+        y = np.array(y)
+        z = np.polyfit(x, y, 3)
+        p = np.poly1d(z)
+        pv = np.polyder(p)
+        print("y="+str(z[0])+"x³+"+str(z[1])+"x²+"+str(z[2])+"x"+str(z[3]))
+        # print("v="+str(z[0]*3)+"x²+"+str(z[1]*2)+"x+"+str(z[2]))
+
+        cv2.imshow('res',frame)
   else:
     input("enter to stop")
     break
